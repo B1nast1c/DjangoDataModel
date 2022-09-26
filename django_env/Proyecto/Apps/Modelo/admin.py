e67eae6fd8e445c1722735fac888b21e9c16ebc7
B1nast1c <79016002+B1nast1c@users.noreply.github.com>
2022-09-25 19:46:06 -0500

    Modelo de Datos / Importaciones

--- django_env/Proyecto/Apps/Modelo/admin.py
+++ django_env/Proyecto/Apps/Modelo/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Postal)
+admin.site.register(Locacion)
+admin.site.register(Telefono)
+admin.site.register(Empresa)
+admin.site.register(categoria_Servicio)
+admin.site.register(Servicio)
+admin.site.register(servicio_Postal)
+admin.site.register(servicio_Empresa)
+admin.site.register(Usuario)
+admin.site.register(Cliente)
+admin.site.register(Admin)
+admin.site.register(Gerente)
+admin.site.register(detalle_Contrato)
+admin.site.register(Contrato)
+admin.site.register(Solicitud)
+admin.site.register(Pago)
+admin.site.register(Transaccion)
